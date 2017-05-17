8c47c743cb3a4c9683a3fd0e650e0a6b6a4159e9
unknown <>
2017-05-17 14:04:10 +0900

    First Commit

---
+++ Assets/2_Script/NumberBlockManegement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberBlockManegement : MonoBehaviour
+{
+    private GameObject[4, 4] numBlockBase;
+
+    private void Awake()
+    {
+    }
+
+    private void Start ()
+    {
+		
+	}
+	
+	private void Update ()
+    {
+		
+	}
+}
