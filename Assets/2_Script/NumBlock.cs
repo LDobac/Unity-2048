b6d06ef9ef3e538292b01921bbf5c96679d56d8e
unknown <>
2017-05-18 19:02:32 +0900

    2017-5-18 수업 시작과 동시에 커밋

---
+++ Assets/2_Script/NumBlock.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class NumBlock : MonoBehaviour
+{
+	private const int basedNumber = 2; 
+
+	private int blockNumber;
+	private Color curColor;
+	private SpriteRenderer spriteRenderer;
+	private Text numberText;
+
+    private void Start()
+	{
+		spriteRenderer = GetComponent<SpriteRenderer>();
+		numberText = transform.GetChild(0).GetChild(0).GetComponent<Text>();
+
+		blockNumber = basedNumber * Random.Range(1,2);
+		numberText.text = blockNumber.ToString();
+
+		curColor = new Color(0.08f + ((float)blockNumber / 10.0f) ,0.05f,0.05f,1.0f);
+	}
+
+	public void CombineBlock(NumBlock other)
+	{
+		blockNumber *= 2;
+		numberText.text = blockNumber.ToString();
+
+		curColor.r += ((float)blockNumber / 10.0f);
+		spriteRenderer.color = curColor;
+	}
+}
--- Assets/2_Script/NumberBlockManegement.cs
+++ Assets/2_Script/NumberBlockManegement.cs
@@ -2,21 +2,82 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class NumberBlockManegement : MonoBehaviour
 {
-    private GameObject[4, 4] numBlockBase;
+    public GameObject numberBlockPrefab;
+
+    private GameObject[,] numBlockBase;
+    private GameObject[,] numBlocks;
 
     private void Awake()
     {
+        numBlockBase = new GameObject[4,4];
+        numBlocks = new GameObject[4,4];
     }
 
     private void Start ()
     {
-		
+		for(int i = 0 ; i < 4 ; i++)
+        {
+            for(int j = 0 ; j < 4 ; j++)
+            {
+                numBlockBase[i,j] = transform.GetChild(i).GetChild(j).gameObject;
+            }
+        }
+
+        GameStart();
 	}
 	
 	private void Update ()
     {
-		
 	}
+
+    public void GameStart()
+    {
+        for(int i = 0 ; i < 2 ;i++)
+        {
+            CreateNumberBlock();
+        }
+    }
+
+    public bool CreateNumberBlock()
+    {
+        if(CheckArrayElementNull(numBlocks))
+        {
+            return false;
+        }
+
+
+        for(;;)
+        {
+            int x = Random.Range(0,4);
+            int y = Random.Range(0,4);
+
+            if(numBlocks[y, x] == null)
+            {
+                numBlocks[y, x] = Instantiate(numberBlockPrefab,numBlockBase[y,x].transform.position,Quaternion.identity);
+
+                break;
+            }
+        }
+
+        return true;
+    }
+
+    private bool CheckArrayElementNull(GameObject[,] array)
+    {
+        for(int i = 0 ; i < array.GetLength(0) ; i++)
+        {
+            for(int j = 0 ; j < array.GetLength(1) ; j++)
+            {
+                if(array[i,j] == null)
+                {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
 }
